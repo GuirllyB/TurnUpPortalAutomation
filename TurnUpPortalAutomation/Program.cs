18ece34a6996ae5b608e4ff70afbd211835f0f87
GUIRLLY BABAO <guirllybabao@gmail.com>
2024-04-09 21:49:39 +1200

    Automated the second test case for TurnUp portal - Create Time Record

--- TurnUpPortalAutomation/Program.cs
+++ TurnUpPortalAutomation/Program.cs
@@ -16,6 +16,7 @@ usernameTextbox.SendKeys("hari");
 IWebElement passwordTextbox = webDriver.FindElement(By.Id("Password"));
 passwordTextbox.SendKeys("123123");
 
+
 //Identify login button and click on the button
 IWebElement loginButton = webDriver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
 loginButton.Click();
@@ -29,4 +30,58 @@ if (helloHariLink.Text == "Hello hari!")
 else
 {
     Console.WriteLine("User hasn't been logged in.");
+}
+
+
+
+//Create a new Time/Material record
+
+//Navigate to Time and Material module (Click Administration button -> Select Time & Materials Option)
+IWebElement administrationDropdown = webDriver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+administrationDropdown.Click();
+IWebElement tmOption = webDriver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+tmOption.Click();
+
+//Click on the Create New Button
+IWebElement createNewButton = webDriver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+createNewButton.Click();
+
+Thread.Sleep(5000);
+
+//Select Time from dropdown
+IWebElement timeTypeCode = webDriver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[2]"));
+timeTypeCode.Click();
+
+//Enter Code
+IWebElement codeTextbox = webDriver.FindElement(By.Id("Code"));
+codeTextbox.SendKeys("ICMarch2024");
+
+//Enter Description
+IWebElement descriptionTextbox = webDriver.FindElement(By.Id("Description"));
+descriptionTextbox.SendKeys("ICMarch2024 Description");
+
+//Enter Price per unit
+IWebElement priceTextbox = webDriver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+priceTextbox.SendKeys("250.00");
+
+//Click on Select file button and select a file
+
+//Click on Save button
+IWebElement saveButton = webDriver.FindElement(By.Id("SaveButton"));
+saveButton.Click();
+
+Thread.Sleep(5000);
+
+//Check if new Time/Material record has been created successfully
+IWebElement goToLastPageButton = webDriver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+goToLastPageButton.Click();
+
+IWebElement newCode.Text = webDriver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+if (newCode.Text == "ICMarch2024")
+{
+    Console.WriteLine("New Time record has been created successfully");
+}
+else
+{
+    Console.WriteLine("New Time record hasn't been created.");
 }
